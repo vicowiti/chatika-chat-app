22cb013c81b6ce562366063a3e2f6d05cf49659c
Victor Owiti <viowiti12@gmail.com>
2022-06-09 19:02:59 +0300

    Added the mongo DB models

---
+++ backend/src/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
